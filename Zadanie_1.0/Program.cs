68580e197cad2b947b209cb442ef19290e18c7fe
Denis <zolotarev1098@gmail.com>
2022-11-30 01:54:15 +0300

    Выполнил первое задание

---
+++ Zadanie_1.0/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите два числа");
+int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+if (a > b) 
+{
+    Console.Write("Max = ");
+    Console.WriteLine(a);
+    Console.Write("Min = ");
+    Console.WriteLine(b);
+}
+else 
+    {
+    Console.Write("Max = ");
+    Console.WriteLine(b);
+    Console.Write("Min = ");
+    Console.WriteLine(a);  
+    }
