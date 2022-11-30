7876ba1e3910165f1cb6f96e17cedbce69f5ca2e
Denis <zolotarev1098@gmail.com>
2022-12-01 01:15:09 +0300

    Выполнил задание 3

---
+++ Zadanie_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = Int32.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{ 
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
