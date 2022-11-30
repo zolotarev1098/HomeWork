7210404b20c4375bc7600fd6e76b3248e6e8ab8d
Denis <zolotarev1098@gmail.com>
2022-12-01 02:21:27 +0300

    Выполнил задание 4

---
+++ Zadanie_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Int32.Parse(Console.ReadLine());
+int i = 1;
+while (i<=a)
+{
+    if (i%2==0)
+    Console.WriteLine(i);
+    i++;
+}
+i++;
