76c2fd0243607b1c255f95c4745de2583c0ad860
Denis <zolotarev1098@gmail.com>
2022-12-03 22:54:20 +0300

    Выполнил задачу 13

---
+++ Zadanie_6_(13)/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n1 = Int32.Parse(Console.ReadLine());
+int n2 = 0;
+int n4 = 1;
+int n7 = n1; 
+if (n1 < 100)
+    Console.WriteLine("Третьей цифры нет");
+else 
+{
+while ( n1 % 10 > 0)
+    {
+    n1 = n1 / 10;
+    n2++;
+    } 
+while (n2 >= 4)
+    {
+    n4 = n4 * 10; 
+    n2--;
+    }
+int n5 = n7 / n4;
+int n6 = n5 % 10; 
+Console.WriteLine(n6);
+}
