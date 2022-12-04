577e550c6268db01d3ac7a114191c86ddf160713
Denis <zolotarev1098@gmail.com>
2022-12-04 13:26:06 +0300

    Выполнил задачу 15

---
+++ Zadanie_7_(15)/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру обозначающую день недели");
+int n = Int32.Parse(Console.ReadLine());
+if (n <= 5)
+   {
+    Console.WriteLine("Рабочий день");
+    }
+else if (n == 6 || n == 7)
+    {
+    Console.WriteLine("Выходной");
+    }
+else if (n > 7)
+{
+Console.WriteLine("В недели 7 дней :)");
+}
+
+ 
