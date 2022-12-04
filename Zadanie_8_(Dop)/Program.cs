a7c2e64f6865831e18a29e75d03b4266a8e7fefa
Denis <zolotarev1098@gmail.com>
2022-12-04 13:29:03 +0300

    Выполнил доп задачу

---
+++ Zadanie_8_(Dop)/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите поседовательность чисел. Что-бы закончить ввод введите 0.");
+int n = Int32.Parse(Console.ReadLine());
+int m = n;
+int n2 = 0; 
+while (n != 0)
+{
+    n = Int32.Parse(Console.ReadLine());
+if (n > m)
+    {
+    n2 = m;
+    m = n;
+    }
+else if (n > n2)
+    n2 = n; 
+}
+Console.WriteLine($"{n2}");
