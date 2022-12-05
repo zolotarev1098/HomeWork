f907ed572b03c9f0c11b59f3521e93be62795fba
Denis <Zolotarev1098@gmail.com>
2022-12-06 00:27:07 +0300

    Выполнил задачу 19

---
+++ Zadanie_9_(19)/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Int32.Parse(Console.ReadLine());
+int[] array = new int[1 + (int)Math.Log10(n)];
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    int digit;
+    n = Math.DivRem(n, 10, out digit);
+    array[i] = digit;
+}
+if (array[0] == array[4] && array[1] == array[3]) 
+    Console.WriteLine("Да");
+else 
+    Console.WriteLine("Нет");
