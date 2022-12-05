86c46d9c42f0eeebde30ca23bd0dd39e55db727d
Denis <Zolotarev1098@gmail.com>
2022-12-06 00:28:31 +0300

    Выполнил задачу 21

---
+++ Zadanie_10_(21)/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите координат x 1-ой точки: ");
+double x_1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координат y 1-ой точки: ");
+double y_1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координат z 1-ой точки: ");
+double z_1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координат x 2-ой точки: ");
+double x_2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координат y 2-ой точки: ");
+double y_2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координат z 2-ой точки: ");
+double z_2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Round(
+    Math.Sqrt(Math.Pow(x_1 - x_2, 2) + Math.Pow(y_1 - y_2, 2) + Math.Pow(z_1-z_2,2)), 2));
