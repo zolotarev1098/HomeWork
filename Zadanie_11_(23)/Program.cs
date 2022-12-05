b5f401b83bb7c2aeac9ab5b9864b67c7f1bd2b71
Denis <Zolotarev1098@gmail.com>
2022-12-06 00:28:51 +0300

    Выполнил задачу 23

---
+++ Zadanie_11_(23)/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n = Int32.Parse(Console.ReadLine());
+int a = 1;
+while (a<=n)
+{
+   double k = Math.Pow(a,3);
+   Console.WriteLine(k);
+   a++;
+}
