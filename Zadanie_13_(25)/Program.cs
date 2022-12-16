3c1e5c070be19633937d259f9314fcc43a701908
Denis <Zolotarev1098@gmail.com>
2022-12-17 01:43:03 +0300

    Выполнил задание 13

---
+++ Zadanie_13_(25)/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа");
+int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+int c = 1;
+for (int i=0; i<b; i++)
+{
+    c= c*a;
+}
+Console.WriteLine(c);
