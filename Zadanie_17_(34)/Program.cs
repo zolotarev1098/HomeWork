0c6f8a3b98284aad03f2cd167507764c1b3931ca
Denis <Zolotarev1098@gmail.com>
2022-12-17 02:11:59 +0300

    Выполнил задание 17

---
+++ Zadanie_17_(34)/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов массива");
+int n = Int32.Parse(Console.ReadLine());
+int[] array = new int [n];
+int c=0;
+for (int i=0; i< array.Length; i++) 
+{
+    array [i] = new Random().Next(100,1000);
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+for (int i=0; i<array.Length; i++) 
+{
+    if (array[i]%2==0)
+    c++;
+}
+Console.WriteLine(c);
