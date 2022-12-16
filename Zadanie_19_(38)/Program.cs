091ad3ae52659a71a21bc881c4b7c0f0bcbb26c0
Denis <Zolotarev1098@gmail.com>
2022-12-17 02:13:11 +0300

    Выполнил задание 19

---
+++ Zadanie_19_(38)/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов массива");
+int n = Int32.Parse(Console.ReadLine());
+double [] array = new double [n];
+double max =0;
+double min = 101;
+for (int i=0; i< array.Length; i++) 
+{
+    array [i] = new Random().Next(-100,101);
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+for (int i=0; i< array.Length; i++) 
+{
+    if (array[i]>max) 
+    max = array[i];
+}
+for (int i=0; i< array.Length; i++) 
+{
+    if (array[i]<min) 
+    min = array[i];
+}
+Console.Write(max-min);
