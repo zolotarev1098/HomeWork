9a67db515443f03f5dfa4a285c6ea0851f0472a8
Denis <Zolotarev1098@gmail.com>
2022-12-17 01:44:29 +0300

    Выполнил задание 15

---
+++ Zadanie_15_(29)/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива");
+int a = Int32.Parse(Console.ReadLine());
+int[] array = new int [a];
+for (int i = 0; i < array.Length; i++)
+ {
+   Console.Write($"Введите {i+1} элемент массива: ");
+   array[i] = Int32.Parse(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
