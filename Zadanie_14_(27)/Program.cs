c41d304ac89e9055cb714da4b35334997e329775
Denis <Zolotarev1098@gmail.com>
2022-12-17 01:43:36 +0300

    Выполнил задание 14

---
+++ Zadanie_14_(27)/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n = Int32.Parse(Console.ReadLine());
+int c = 0;  
+int[] array = new int[1 + (int)Math.Log10(n)];
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    int digit;
+    n = Math.DivRem(n, 10, out digit);
+    array[i] = digit;
+}
+for(int a=0; a<array.Length; a++)
+    {
+        c = c + array[a]; 
+    }
+Console.WriteLine(c);
+
