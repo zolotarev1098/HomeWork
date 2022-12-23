83e2600484d413c1d60776b406aeeee118aca349
Denis <Zolotarev1098@gmail.com>
2022-12-23 19:33:17 +0300

    Выполнил задание 20(41)

---
+++ Zadanie_20_(41)/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите последовательность чисел через запятую: ");
+int[] array = Console.ReadLine().Split(",").Select(x => int.Parse(x)).ToArray();
+int a (int []array) 
+{
+    int i =0;
+    foreach(int element in array) 
+    {
+        if (element >0 )
+        i++;
+    }
+    return i;
+}
+
+Console.Write(a(array));
