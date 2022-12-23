15444fca6159b5599c0d6efda300c72526e722c2
Denis <Zolotarev1098@gmail.com>
2022-12-23 19:34:08 +0300

    Выполнил задание 21(43)

---
+++ Zadanie_21_(43)/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Прямые заданы уравнениями: y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x=(b2-b1)/(k1-k2);
+double y = k1*x +b1;
+Console.Write($"({x};{y})");
