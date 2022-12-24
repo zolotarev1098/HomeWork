7ef234c036c39937cd68a07f2f8a1fd3df422223
Denis <Zolotarev1098@gmail.com>
2022-12-25 01:06:40 +0300

    Выполил задание 24(52)

---
+++ Zadanie_24_(52)/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк массива m: ");
+int m = Int32.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива n: ");
+int n = Int32.Parse(Console.ReadLine());
+int [,] array = new int [m,n];
+double [] sum = new double [n];
+double [] x = new double [n];
+void M (int[,] array) 
+{
+for (int i=0; i<array.GetLength(0); i++) 
+{
+    for (int j=0; j<array.GetLength(1); j++) 
+    {
+        array[i,j] = new Random().Next(0, 11);
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+M (array);
+ for (int j=0; j<array.GetLength(1); j++)
+ {
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        sum [j]+= array[i,j];
+    }
+    x [j]= Math.Round(sum[j]/array.GetLength(0),2);
+    }
+Console.WriteLine($"[{string.Join("    ", x)}]");
