1c32945b6d543593e7d7c8b48c5e7b59eff77644
Denis <Zolotarev1098@gmail.com>
2022-12-25 01:05:32 +0300

    Выполил задание 22(47)

---
+++ Zadanie_22_(47)/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк массива m: ");
+int m = Int32.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива n: ");
+int n = Int32.Parse(Console.ReadLine());
+double[,] array = new double [m,n];
+for (int i=0; i<array.GetLength(0); i++) 
+{
+    for (int j=0; j<array.GetLength(1); j++) 
+    {
+        array[i,j] = new Random().NextDouble() * (10 + 10) - 10;
+        array[i,j] = Math.Round(array[i,j],2);
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
